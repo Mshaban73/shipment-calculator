ad8a382a7731513076b058c60b3b5c0b85cbb87f
Mshaban73 <mshaban80@gmail.com>
2025-10-25 04:29:41 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
-
 import React from 'react';
 
-export const Footer: React.FC = () => (
-  <footer className="bg-white mt-12 print:hidden">
+interface FooterProps {
+  className?: string; // Prop to accept className
+}
+
+export const Footer: React.FC<FooterProps> = ({ className }) => (
+  // Apply the passed className here
+  <footer className={`bg-white mt-12 ${className}`}>
     <div className="container mx-auto py-4 px-4 text-center text-slate-500">
       <p>&copy; {new Date().getFullYear()} جميع الحقوق محفوظة شركة المتحدة جروب AHK</p>
     </div>
